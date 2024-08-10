f0525d031c8b817e4c5a5859b970aaa78d9764f8
muhammedElnashar <mohamedelnashar01@gmail.com>
2024-08-10 15:44:57 +0300

    Project Design

---
+++ layouts/navbar.php
@@ -0,0 +1,15 @@
+<nav id="navbar" class="navbar py-3 w-100  mt-1 d-none d-lg-block">
+    <div class="  d-flex  justify-content-between ">
+        <div class="ms-5 h6 fw-bold">
+            <a href="#" class="text-white text-decoration-none  "><small class="text-white  mx-2">Home</small></a>
+            <a href="#" class="text-white text-decoration-none  "><small class="text-white  mx-2">Products</small></a>
+            <a href="#" class="text-white text-decoration-none  "><small class="text-white  mx-2">Users</small></a>
+            <a href="#" class="text-white text-decoration-none  "><small class="text-white  mx-2">Manual Order</small></a>
+            <a href="#" class="text-white text-decoration-none  "><small class="text-white  mx-2">Checks</small></a>
+        </div>
+        <div class="me-5" >
+            <a href="#" class="text-white text-decoration-none  "><small class="text-white  mx-2">Admin</small></a>
+            <img class="mx-3 rounded-1" src="img.png" width="40px" height="50px"  alt="" style="margin-top: -12px; margin-bottom: -10px;" >
+        </div>
+    </div>
+</nav>
---
+++ layouts/sidebar.php
@@ -0,0 +1,105 @@
+<div class="wrapper">
+    <aside id="sidebar">
+        <div class="d-flex">
+            <button id="toggle-btn" type="button">
+                <i class="lni lni-grid-alt"></i>
+            </button>
+            <div class="sidebar-logo">
+                <a href="#">Cafeteria</a>
+            </div>
+        </div>
+        <ul class="sidebar-nav">
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="lni lni-users"></i>
+                    <span>Profile</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="lni lni-users"></i>
+                    <span>Users</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="lni lni-users"></i>
+                    <span>Product</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="lni lni-users"></i>
+                    <span>Category</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link has-dropdown collapsed" data-bs-toggle="collapse" data-bs-target="#auth"
+                   aria-expanded="false" aria-controls="auth">
+                    <i class="lni lni-protection"></i>
+                    <span>Auth</span>
+                </a>
+                <ul id="auth" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">Login</a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">register</a>
+                    </li>
+
+                </ul>
+                <!--  </li>
+                  <li class="sidebar-item">
+                      <a href="#" class="sidebar-link has-dropdown collapsed" data-bs-toggle="collapse" data-bs-target="#multi" aria-expanded="false" aria-controls="multi">
+                          <i class="lni lni-layouts"></i>
+                          <span>multi</span>
+                      </a>
+                      <ul id="multi" class="sidebar-dropdown list-unstyled collapse"  data-bs-parent="#sidebar">
+                          <li class="sidebar-item">
+                              <a href="#" class="sidebar-link has-dropdown collapsed" data-bs-toggle="collapse" data-bs-target="#multi-two" aria-expanded="false" aria-controls="multi-two">
+                              Two LInks
+                              </a>                    </li>
+                          <li class="sidebar-item">
+                              <a href="#" class="sidebar-link">Multi 2</a>
+                          </li>
+                          <li class="sidebar-item">
+                              <a href="#" class="sidebar-link">Multi 3</a>
+                          </li>
+                          <li class="sidebar-item">
+                              <a href="#" class="sidebar-link">Multi 4</a>
+                          </li>
+                          <li class="sidebar-item">
+                              <a href="#" class="sidebar-link">Multi 5</a>
+                          </li>
+                          <li class="sidebar-item">
+                              <a href="#" class="sidebar-link">Multi 6</a>
+                          </li>
+                          <li class="sidebar-item">
+                              <a href="#" class="sidebar-link">Multi
+                      </ul>
+                  </li>-->
+
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="lni lni-popup"></i>
+                    <span>Notifications</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="lni lni-cog"></i>
+                    <span>setting</span>
+                </a>
+            </li>
+
+        </ul>
+        <div class="sidebar-footer">
+            <a href="#" class="sidebar-link">
+                <i class="lni lni-exit"></i>
+                <span>Logout</span>
+            </a>
+        </div>
+    </aside>
+
+
+
