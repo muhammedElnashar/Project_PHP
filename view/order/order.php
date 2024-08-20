b6d5ffc47cf8c346bf668d6d9e057b5bedfef581
Mohamed <mohamed.ashraf74@gmail.com>
2024-08-20 22:02:25 +0300

    Update admin order

--- view/order/order.php
+++ view/order/order.php
@@ -36,7 +36,31 @@
 
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+<<<<<<< Updated upstream
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="../../assets/script.js"></script>
 </body>
 </html>
+=======
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+<script src="../../assets/script.js"></script>
+<script>
+    function changeStatus(order_id) {
+        alert(order_id)
+        $.ajax({
+            url: 'changeStatus.php',
+            type: 'POST',
+            data: {order_id: order_id},
+            success: function () {
+                alert('Status updated successfully');
+            },
+            error: function () {
+                alert('Error updating status');
+            }
+        })
+    }
+</script>
+>>>>>>> Stashed changes
