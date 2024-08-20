1e1be0cf7a67da53b07f324d50afd65128511c95
muhammedElnashar <mohamedelnashar01@gmail.com>
2024-08-20 17:04:55 +0300

    Update Layouts

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -1,3 +1,5 @@
+<?php
+if ($_SESSION['permission'] == 1): ?>
 <div class="wrapper">
     <aside id="sidebar">
         <div class="d-flex">
@@ -9,12 +11,6 @@
             </div>
         </div>
         <ul class="sidebar-nav">
-            <li class="sidebar-item">
-                <a href="#" class="sidebar-link">
-                    <i class="lni lni-users"></i>
-                    <span>Profile</span>
-                </a>
-            </li>
             <li class="sidebar-item">
                 <a href="#" class="sidebar-link">
                     <i class="lni lni-users"></i>
@@ -37,69 +33,64 @@
                 <a href="#" class="sidebar-link has-dropdown collapsed" data-bs-toggle="collapse" data-bs-target="#auth"
                    aria-expanded="false" aria-controls="auth">
                     <i class="lni lni-protection"></i>
-                    <span>Auth</span>
+                    <span>Order</span>
                 </a>
                 <ul id="auth" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
                     <li class="sidebar-item">
-                        <a href="#" class="sidebar-link">Login</a>
+                        <a href="#" class="sidebar-link">Create Order</a>
                     </li>
                     <li class="sidebar-item">
-                        <a href="#" class="sidebar-link">register</a>
+                        <a href="#" class="sidebar-link">Checks</a>
                     </li>
 
                 </ul>
-                <!--  </li>
-                  <li class="sidebar-item">
-                      <a href="#" class="sidebar-link has-dropdown collapsed" data-bs-toggle="collapse" data-bs-target="#multi" aria-expanded="false" aria-controls="multi">
-                          <i class="lni lni-layouts"></i>
-                          <span>multi</span>
-                      </a>
-                      <ul id="multi" class="sidebar-dropdown list-unstyled collapse"  data-bs-parent="#sidebar">
-                          <li class="sidebar-item">
-                              <a href="#" class="sidebar-link has-dropdown collapsed" data-bs-toggle="collapse" data-bs-target="#multi-two" aria-expanded="false" aria-controls="multi-two">
-                              Two LInks
-                              </a>                    </li>
-                          <li class="sidebar-item">
-                              <a href="#" class="sidebar-link">Multi 2</a>
-                          </li>
-                          <li class="sidebar-item">
-                              <a href="#" class="sidebar-link">Multi 3</a>
-                          </li>
-                          <li class="sidebar-item">
-                              <a href="#" class="sidebar-link">Multi 4</a>
-                          </li>
-                          <li class="sidebar-item">
-                              <a href="#" class="sidebar-link">Multi 5</a>
-                          </li>
-                          <li class="sidebar-item">
-                              <a href="#" class="sidebar-link">Multi 6</a>
-                          </li>
-                          <li class="sidebar-item">
-                              <a href="#" class="sidebar-link">Multi
-                      </ul>
-                  </li>-->
-
-            <li class="sidebar-item">
-                <a href="#" class="sidebar-link">
-                    <i class="lni lni-popup"></i>
-                    <span>Notifications</span>
-                </a>
-            </li>
-            <li class="sidebar-item">
-                <a href="#" class="sidebar-link">
-                    <i class="lni lni-cog"></i>
-                    <span>setting</span>
-                </a>
             </li>
 
         </ul>
         <div class="sidebar-footer">
-            <a href="#" class="sidebar-link">
+            <a href="../Login/logout.php" class="sidebar-link">
                 <i class="lni lni-exit"></i>
                 <span>Logout</span>
             </a>
         </div>
     </aside>
 
+    <?php else: ?>
 
 
+    <div class="wrapper">
+        <aside id="sidebar">
+            <div class="d-flex">
+                <button id="toggle-btn" type="button">
+                    <i class="lni lni-grid-alt"></i>
+                </button>
+                <div class="sidebar-logo">
+                    <a href="#">Cafeteria</a>
+                </div>
+            </div>
+            <ul class="sidebar-nav">
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="lni lni-users"></i>
+                        <span>Create Order</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="lni lni-users"></i>
+                        <span>My Orders</span>
+                    </a>
+                </li>
+
+
+            </ul>
+            <div class="sidebar-footer">
+                <a href="../Login/logout.php" class="sidebar-link">
+                    <i class="lni lni-exit"></i>
+                    <span>Logout</span>
+                </a>
+            </div>
+        </aside>
+
+
+        <?php endif; ?>
