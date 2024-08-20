c1f2568641f6307512ebc3aff0807a8c82192b17
Mohamed <mohamed.ashraf74@gmail.com>
2024-08-20 22:10:51 +0300

    Update admin order

--- view/order/order.php
+++ view/order/order.php
@@ -153,12 +153,10 @@ if ($db) {
 
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-<<<<<<< Updated upstream
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="../../assets/script.js"></script>
 </body>
 </html>
-=======
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
@@ -180,4 +178,3 @@ if ($db) {
         })
     }
 </script>
->>>>>>> Stashed changes
